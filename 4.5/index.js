5a1897a1ea85d1e90cb5a0e05e03ae56074636fc
yizhi_iMac <wu-xuesong@qq.com>
2018-11-26 12:35:56 +0800

    update 4.5

--- 4.5/index.js
+++ 4.5/index.js
@@ -1,30 +1,54 @@
 // 字符串
 function processString() {
+  // 快速获取正则匹配数据
+  const raw = '共找到16条记录，耗时30秒。'
+  const [nothing, total, time] = raw.match(/共找到(\d+)条记录，耗时(\d+)秒。/) || []
+  console.log(total, time)
 
+  // 获取请求头的用户token
+  const header = 'Authorization: Bearer ey23423sdafsadfwerer '
+  console.log(header.split(' ').filter(v => v).pop())
 }
-processString()
+// processString()
 
 // 数组
 function processArray() {
-  const raw = ['Node.js', 'Vue.js', 'Express.js', false]
-  const ret = raw.filter(v => v).map((v, k) => ({
-    id: k + 1,
-    name: v
-  }))
+  const raw = [
+    { name: 'node', views: 400 },
+    { name: 'vue', views: 600 },
+    { name: 'express', views: 500 },
+  ]
+  const ret = raw.reduce(
+    (acc, v) => {
+      acc += v.views
+      return acc
+    },
+    0
+  ).concat([
+    { name: 'koa', views: 300 }
+  ])
   console.log(ret)
 }
-processArray()
+// processArray()
 
 // 对象
 function processObject() {
-  const raw = {
-    node: 'Node.js',
-    vue: 'Vue.js',
-    express: 'Express.js',
+  const defaults = {
+    created: '已下单',
+    paid: '支付',
   }
-  const ret = Object.entries(raw).reduce((acc, v) => {
-    // acc.push({text: })
-    return acc
-  }, [])
+
+  // 合并多个对象
+  const options = Object.assign({}, defaults, {
+    paid: '已支付',
+    finished: '已完成'
+  })
+  console.log(options)
+
+  // 对象转成可扩展的数组数据
+  const listData = Object.entries(options).map(([value, text]) => ({
+    value, text
+  }))
+  console.log(listData)
 }
-processObject()
+// processObject()
